0e284a85755df668dec29615c6fa57b5631e9195
mouseorcleg <torindubozit@gmail.com>
2023-01-06 11:42:20 +0000

    Color theme + icon set up

---
+++ Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  TomatoTomato2
+//
+//  Created by Maria Kharybina on 06/01/2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+    
+    
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let extra = Color("ExtraColor")
+}
