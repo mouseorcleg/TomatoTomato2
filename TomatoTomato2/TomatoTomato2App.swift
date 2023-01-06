d6a5ff6e1a05997a6e418637fea100555e2a2097
mouseorcleg <torindubozit@gmail.com>
2023-01-06 10:27:07 +0000

    Initial Commit

---
+++ TomatoTomato2/TomatoTomato2App.swift
@@ -0,0 +1,17 @@
+//
+//  TomatoTomato2App.swift
+//  TomatoTomato2
+//
+//  Created by Maria Kharybina on 06/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TomatoTomato2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
